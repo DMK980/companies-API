2e21649981ede8897f9faf7f6eab34f70d60c2db
DMK980 <derrick.mutuma@gmail.com>
2024-05-13 11:27:02 +0100

    adding data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-import scraper
+import pandas as pd
+import json
 
 # setting app flask backend
 app = Flask(__name__)
@@ -7,7 +8,11 @@ app = Flask(__name__)
 # route for companies list
 @app.route("/",methods={"GET"})
 def companies_list():
-    return scraper.data_json["0"]
+    data = pd.read_csv("2024-05-10_-_Worker_and_Temporary_Worker.csv")
+    data_str = data.to_json(orient="index")
+    data_json = json.loads(data_str)
+
+    return data_json
 
 if __name__ == "__main__":
     app.run(debug=True)
